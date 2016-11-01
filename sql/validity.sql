364e884dcf087ad21a73f518cefb1adc03f98973
eric-pommereau <eric.pommereau@gmail.com>
2016-11-01 10:09:23 +0100

    export shp communes algerie

---
+++ sql/validity.sql
@@ -0,0 +1,4 @@
+﻿-- Vérifier la validité des polygons
+select osm_id, name, ST_IsValidDetail(wkb_geometry) as unval_reason from multipolygons 
+where admin_level = '7' and ST_IsValid(wkb_geometry) = false;
+
